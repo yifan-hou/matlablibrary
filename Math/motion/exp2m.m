747507610bda49529b6a36e7e69b07ce08c7fe36
Yifan <yifanh@cmu.edu>
2018-11-29 18:23:29 -0500

    symbolic-friendly exp2m

--- Math/motion/exp2m.m
+++ Math/motion/exp2m.m
@@ -1,8 +1,12 @@
-% n: 3x1 vector
+% exponential coordinate to rotation matrix
+% (Rodriguez's formula)
+%
+% n: 3xk vector
 function R = exp2m(n)
 
-theta = normByCol(n);
-n = bsxfun(@rdivide, n, theta);
+theta  = normByCol(n);
+theta_ = ones(3,1)*theta;
+n      = n./theta_;
 
 cw = [0 -n(3) n(2) ; n(3) 0 -n(1) ; -n(2) n(1) 0 ];
 
