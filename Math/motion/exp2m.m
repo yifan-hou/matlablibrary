4fb9d03fd925197694ecd6f06d1d8b5854b1980b
Yifan <yifanh@cmu.edu>
2018-11-30 17:23:21 -0500

    right way to do symbolic with e2w_m, exp2m

--- Math/motion/exp2m.m
+++ Math/motion/exp2m.m
@@ -3,12 +3,17 @@
 %
 % n: 3x1 vector
 function R = exp2m(n)
-
 theta  = norm(n);
-if theta > 1e-10
-    n  = n/theta;
-    cw = [0 -n(3) n(2) ; n(3) 0 -n(1) ; -n(2) n(1) 0 ];
-    R  = eye(3) + cw*sin(theta) + cw*cw*(1-cos(theta));
+if isnumeric(n)
+	if theta > 1e-10
+	    n  = n/theta;
+	    cw = [0 -n(3) n(2) ; n(3) 0 -n(1) ; -n(2) n(1) 0 ];
+	    R  = eye(3) + cw*sin(theta) + cw*cw*(1-cos(theta));
+	else
+	    R = eye(3);
+	end
 else
-    R = eye(3);
+	n  = n/theta;
+	cw = [0 -n(3) n(2) ; n(3) 0 -n(1) ; -n(2) n(1) 0 ];
+	R = eye(3) + cw*sin(theta) + cw*cw*(1-cos(theta));
 end
